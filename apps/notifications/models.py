af0cbae90555e8c15787c3a1979049eeab74df1d
markmutinda <pollomarco2002@gmail.com>
2026-04-29 12:05:38 +0300

    fixed celery worker

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.utils import timezone
+from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from apps.core.models import AuditLog
 
